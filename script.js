735aaf53a5ce2a5b9707dfeb2ebd0c7edb971cde
Sofiia <sofiia.yulevych@gmail.com>
2025-11-16 15:16:36 +0100

    html + css

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 fetch("get_events.php")
     .then(res => res.json())
     .then(events => {
-        console.log(events);
+        console.log("Events:", events);
         const list = document.getElementById("event-list");
 
         events.forEach(ev => {
             const div = document.createElement("div");
             div.className = "event";
 
+            console.log("Vor innerHTML - div:", div); // Debug 1
+
             div.innerHTML = `
-              <h3>${ev.name}</h3>
+              <div class="event-header">
+                <h3>${ev.name}</h3>
+              </div>
               <p>${ev.description || ""}</p>
+              <p>Text</p>
               <p><b>Von:</b> ${ev.start_date}</p>
               <p><b>Bis:</b> ${ev.end_date}</p>
-          `;
+            `;
+
+            console.log("Nach innerHTML - div content:", div.innerHTML); // Debug 2
+            console.log("Event-Header div:", div.querySelector('.event-header')); // Debug 3
 
             list.appendChild(div);
         });
